ca2dc64a3448d64759d8a08b32184c496c2063c3
Ruen-Duo.Lee <ruenduo.lee@linecorp.com>
2023-03-15 17:34:36 +0800

    Extract method

--- src/budgetService.js
+++ src/budgetService.js
@@ -6,18 +6,19 @@ export class BudgetService {
   monthMap = {}
   budget = 0
 
+  getDayOfMonth(_budget, format) {
+    return dayjs(_budget.YearMonth, format).daysInMonth();
+  }
+
   query(start, end) {
     let budgetRepo = new BudgetRepo();
-    const allBudget = budgetRepo.getAll();
-
-    function getDayOfMonth(_budget, format) {
-      return dayjs(_budget.YearMonth, format).daysInMonth();
-    }
+    const allBudget = this.queryDB(budgetRepo);
 
     allBudget.forEach((_budget) => {
-      const budgetFor1Day = _budget.Amount / getDayOfMonth(_budget, 'YYYYMM')
+      const budgetFor1Day = _budget.Amount / this.getDayOfMonth(_budget, 'YYYYMM')
       this.monthMap[_budget.YearMonth] = budgetFor1Day
     })
+
     let loop = new Date(start)
     while (loop <= new Date(end)) {
       let curr = dayjs(loop).format('YYYYMM')
@@ -28,4 +29,7 @@ export class BudgetService {
     return this.budget
   }
 
+  queryDB(budgetRepo) {
+    return budgetRepo.queryDB();
+  }
 }
